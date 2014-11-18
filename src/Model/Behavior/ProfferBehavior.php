3566b8f957a882ac4e96efe325d5a08a81a71a2d
David Yell <dyell@ukwebmedia.com>
2014-11-18 15:46:09 +0000

    Fixed the generation of thumbnails

--- src/Model/Behavior/ProfferBehavior.php
+++ src/Model/Behavior/ProfferBehavior.php
@@ -1,16 +1,28 @@
 <?php
+/**
+ * Proffer
+ * An upload behavior plugin for CakePHP 3
+ *
+ * @author David Yell <neon1024@gmail.com>
+ */
+
 namespace Proffer\Model\Behavior;
 
 use ArrayObject;
-use Cake\Core\Plugin;
 use Cake\Event\Event;
 use Cake\Network\Exception\BadRequestException;
 use Cake\ORM\Behavior;
 use Cake\ORM\Entity;
 use Cake\ORM\Table;
 use Cake\Utility\String;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\ImagineInterface;
+use Imagine\Imagick\Imagine as Imagick;
+use Imagine\Gd\Imagine as Gd;
+use Imagine\Gmagick\Imagine as Gmagick;
+use Imagine\Image\Point;
 use Imagine\Image\Box;
-use Imagine\Imagick\Imagine;
+use Imagine\Filter\Transformation;
 
 /**
  * Proffer behavior
@@ -25,6 +37,46 @@ class ProfferBehavior extends Behavior {
 	protected $_defaultConfig = [];
 
 /**
+ * Store our instance of Imagine
+ *
+ * @var ImagineInterface $Imagine
+ */
+	private $Imagine;
+
+/**
+ * Constructor hook
+ *
+ * @param array $config
+ */
+	public function initialize(array $config) {
+	}
+
+/**
+ * Get the specified Imagine engine class
+ *
+ * @return ImagineInterface
+ */
+	protected function getImagine() {
+		return $this->Imagine;
+	}
+
+/**
+ * Set the Imagine engine class
+ *
+ * @param string $engine
+ * @return void
+ */
+	protected function setImagine($engine = 'imagick') {
+		if ($engine === 'gd') {
+			$this->Imagine = new Gd();
+		} elseif ($engine === 'gmagick') {
+			$this->Imagine = new Gmagick();
+		}
+
+		$this->Imagine = new Imagick();
+	}
+
+/**
  * beforeSave method
  *
  * @param Event $event
@@ -33,9 +85,6 @@ class ProfferBehavior extends Behavior {
  * @return bool
  */
 	public function beforeSave(Event $event, Entity $entity, ArrayObject $options) {
-		/* @var \Cake\ORM\Table $table */
-		$table = $event->subject();
-
 		foreach ($this->config() as $field => $settings) {
 			if ($entity->has($field) && is_array($entity->get($field)) && $entity->get($field)['error'] === UPLOAD_ERR_OK) {
 
@@ -43,18 +92,47 @@ class ProfferBehavior extends Behavior {
 					throw new BadRequestException('File must be uploaded using HTTP post.');
 				}
 
-				$path = $this->buildPath($table, $entity, $field);
+				$path = $this->buildPath($this->_table, $entity, $field);
 
 				if (move_uploaded_file($entity->get($field)['tmp_name'], $path['full'])) {
 					$entity->set($field, $entity->get($field)['name']);
 					$entity->set($settings['dir'], $path['parts']['seed']);
 
+					$this->setImagine($this->config($field)['thumbnailMethod']);
 					$this->makeThumbs($field, $path);
 				}
 			}
 		}
 
 		return true;
+	}
+
+/**
+ * Generate the defined thumbnails
+ *
+ * @param string $field The name of the upload field
+ * @param string $path The path array
+ * @return void
+ */
+	protected function makeThumbs($field, $path) {
+		$image = $this->Imagine->open($path['full']);
+		foreach ($this->config($field)['thumbnailSizes'] as $prefix => $thumbSize) {
+			$filePath = $path['parts']['root'] . DS . $path['parts']['table'] . DS . $path['parts']['seed'] . DS . $prefix . '_' . $path['parts']['name'];
+
+			$event = new Event('Proffer.beforeThumbs', $this->_table, [$image]);
+			$this->_table->eventManager()->dispatch($event);
+
+			if (isset($thumbSize['crop']) && $thumbSize['crop'] === false) {
+				$image = $this->thumbnailCropScale($image, $thumbSize['w'], $thumbSize['h']);
+			} else {
+				$image = $this->thumbnailScale($image, $thumbSize['w'], $thumbSize['h']);
+			}
+
+			$event = new Event('Proffer.afterThumbs', $this->_table, [$image]);
+			$this->_table->eventManager()->dispatch($event);
+
+			$image->save($filePath);
+		}
 	}
 
 /**
@@ -78,7 +156,8 @@ class ProfferBehavior extends Behavior {
 		$path['name'] = $entity->get($field)['name'];
 
 		$fullPath = implode(DS, $path);
-		if (!file_exists($fullPath)) {
+
+		if (!file_exists($path['root'] . DS . $path['table'] . DS . $path['seed'] . DS)) {
 			mkdir($path['root'] . DS . $path['table'] . DS . $path['seed'] . DS, 0777, true);
 		}
 
@@ -86,28 +165,42 @@ class ProfferBehavior extends Behavior {
 	}
 
 /**
- * Generate the defined thumbnails
+ * Scale an image to best fit a thumbnail size
  *
- * @param $field The name of the upload field
- * @param $path The path array
- * @return void
+ * @param \Imagine\Image\ImageInterface $image
+ * @param int $width The width in pixels
+ * @param int $height The height in pixels
+ * @return \Imagine\Image\ImageInterface
  */
-	protected function makeThumbs($field, $path) {
-		$imagine = new Imagine();
-		$image = $imagine->open($path['full']);
-		foreach ($this->config($field)['thumbnailSizes'] as $prefix => $thumbSize) {
-			$filePath = $path['parts']['root'] . DS . $path['parts']['table'] . DS . $path['parts']['seed'] . DS . $prefix . '_' . $path['parts']['name'];
+	protected function thumbnailScale($image, $width, $height) {
+		$transformation = new Transformation();
+		$transformation->thumbnail(new Box($width, $height));
+		return $transformation->apply($image);
+	}
 
-			// Both sizes, box resize
-			if (isset($thumbSize['w']) && isset($thumbSize['h'])) {
-				$image->resize(new Box($thumbSize['w'], $thumbSize['h']));
-			} elseif (isset($thumbSize['w']) && !isset($thumbSize['h'])) {
-				$image->resize($image->getSize()->widen($thumbSize['w']));
-			} elseif (!isset($thumbSize['w']) && isset($thumbSize['h'])) {
-				$image->resize($image->getSize()->heighten($thumbSize['h']));
-			}
+/**
+ * Create a thumbnail by scaling an image and cropping it to fit the exact dimensions
+ *
+ * @param \Imagine\Image\ImageInterface $image
+ * @param int $targetWidth The width in pixels
+ * @param int $targetHeight The height in pixels
+ * @return \Imagine\Image\ImageInterface
+ */
+	protected function thumbnailCropScale($image, $targetWidth, $targetHeight) {
+		$target = new Box($targetWidth, $targetHeight);
+		$sourceSize = $image->getSize();
 
-			$image->save($filePath);
+		if ($sourceSize->getWidth() > $sourceSize->getHeight()) {
+			$width = $sourceSize->getWidth() * ($target->getHeight() / $sourceSize->getHeight());
+			$cropPoint = new Point((int)(max($width - $target->getWidth(), 0) / 2), 0);
+		} else {
+			$height = $sourceSize->getHeight() * ($target->getWidth() / $sourceSize->getWidth());
+			$cropPoint = new Point(0, (int)(max($height - $target->getHeight(), 0) / 2));
 		}
+
+		$box = new Box($targetWidth, $targetHeight);
+
+		return $image->thumbnail($box, ImageInterface::THUMBNAIL_OUTBOUND)
+			->crop($cropPoint, $target);
 	}
 }
