4327518e3439fe4da442841a75ef933ed9a47de9
David Yell <dyell@ukwebmedia.com>
2014-11-21 11:56:00 +0000

    Created some basic validation rules

--- src/Model/Validation/ProfferRules.php
+++ src/Model/Validation/ProfferRules.php
@@ -8,16 +8,107 @@
 namespace Proffer\Model\Validation;
 
 use Cake\Validation\Validator;
+use finfo;
 
 class ProfferRules extends Validator {
 
-	public static function filesize($value, array $context) {
-		var_dump(__METHOD__ . ' in ' . __FILE__);
-		var_dump(func_get_args());
-		exit;
+/**
+ * Check the size of the image
+ *
+ * @param array $value
+ * @param int $size Filesize in bytes
+ * @param array $context
+ * @return bool
+ */
+	public static function filesize($value, $size, array $context) {
+		if ($value['size'] <= $size) {
+			return true;
+		}
+
+		return false;
 	}
 
-	public static function extension($value, array $context) {
-		var_dump(__METHOD__ . ' in ' . __FILE__);
+/**
+ * Make sure the extension matches the allowed
+ *
+ * @param array $value
+ * @param array $extensions Array of file extensions to allow
+ * @param array $context
+ * @return bool
+ */
+	public static function extension($value, array $extensions, array $context) {
+		$extension = pathinfo($value['tmp_name'], PAHTINFO_EXTENSION);
+
+		if (in_array($extension, $extensions)) {
+			return true;
+		}
+
+		return false;
+	}
+
+/**
+ * Check the mimetype of the file
+ *
+ * For a full list of mime types
+ * http://www.sitepoint.com/web-foundations/mime-types-complete-list/
+ *
+ * @param array $value
+ * @param array $types An array of mime type strings to match
+ * @param array $context
+ * @return bool
+ */
+	public static function mimetype($value, array $types, array $context) {
+		$finfo = new finfo();
+		$type = $finfo->file($value['tmp_name'], FILEINFO_MIME_TYPE);
+
+		if (in_array($type, $types)) {
+			return true;
+		}
+
+		return false;
+	}
+
+/**
+ * Validate the dimensions of an image. If the file isn't an image then validation will fail
+ *
+ * @param array $value
+ * @param array $dimensions Array of rule dimensions for example
+ * ['dimensions', [
+ * 		'min' => ['w' => 100, 'h' => 100],
+ * 		'max' => ['w' => 500, 'h' => 500]
+ * ]]
+ * would validate a minimum size of 100x100 pixels and a maximum of 500x500 pixels
+ * @param array $context
+ * @return bool
+ */
+	public static function dimensions($value, array $dimensions, array $context) {
+		$fileDimensions = getimagesize($value['tmp_name']);
+
+		if ($fileDimensions === false) {
+			return false;
+		}
+
+		$sourceWidth = $fileDimensions[0];
+		$sourceHeight = $fileDimensions[1];
+
+		foreach ($dimensions as $rule => $sizes) {
+			if ($rule === 'min') {
+				if (isset($sizes['w']) && $sourceWidth < $sizes['w']) {
+					return false;
+				}
+				if (isset($sizes['h']) && $sourceHeight < $sizes['h']) {
+					return false;
+				}
+			} elseif ($rule === 'max') {
+				if (isset($sizes['w']) && $sourceWidth > $sizes['w']) {
+					return false;
+				}
+				if (isset($sizes['h']) && $sourceHeight > $sizes['h']) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 }
