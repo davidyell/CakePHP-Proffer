fa21717121faab792dfc4338bf6fbcb24be414d7
David Yell <dyell@ukwebmedia.com>
2014-11-25 12:40:04 +0000

    Relates to #11. Defined the cake constant, so Travis can find basics.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,5 @@
 // Customize this to be a relative path for embedded plugins.
 // For standalone plugins, this should point at a CakePHP installation.
 require 'vendor/autoload.php';
+define(CAKE, 'vendor/cakephp/cakephp/src/');
 require 'vendor/cakephp/cakephp/config/bootstrap.php';
