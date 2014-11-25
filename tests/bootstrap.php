5d0458f96160a4f454999450fb3d6dc4a7fbe15a
David Yell <dyell@ukwebmedia.com>
2014-11-25 14:42:40 +0000

    Relates to #11. Still trying to get bootstrap working

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,83 +1,104 @@
 <?php
-function find_root() {
-	$root = dirname(__DIR__);
-	if (is_dir($root . '/vendor/cakephp/cakephp')) {
-		return $root;
-	}
+use Cake\Cache\Cache;
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+use Cake\Log\Log;
 
-	$root = dirname(dirname(__DIR__));
-	if (is_dir($root . '/vendor/cakephp/cakephp')) {
-		return $root;
-	}
+require_once 'vendor/autoload.php';
 
-	$root = dirname(dirname(dirname(__DIR__)));
-	if (is_dir($root . '/vendor/cakephp/cakephp')) {
-		return $root;
-	}
-}
-
+// Path constants to a few helpful things.
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', find_root());
-define('APP_DIR', 'App');
-define('WEBROOT_DIR', 'webroot');
-define('APP', ROOT . '/tests/App/');
-define('CONFIG', ROOT . '/tests/config/');
-define('WWW_ROOT', ROOT . DS . WEBROOT_DIR . DS);
-define('TESTS', ROOT . DS . 'tests' . DS);
-define('TMP', ROOT . DS . 'tmp' . DS);
-define('LOGS', TMP . 'logs' . DS);
-define('CACHE', TMP . 'cache' . DS);
-define('CAKE_CORE_INCLUDE_PATH', ROOT . '/vendor/cakephp/cakephp');
-define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+define('ROOT', dirname(__DIR__) . DS);
+define('CAKE_CORE_INCLUDE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
+define('CORE_PATH', ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS);
 define('CAKE', CORE_PATH . 'src' . DS);
+define('APP', ROOT . 'tests' . DS . 'test_app' . DS);
+define('APP_DIR', 'test_app');
+define('WEBROOT_DIR', 'webroot');
+define('WWW_ROOT', APP . 'webroot' . DS);
+define('TMP', ROOT . 'tests' . DS . 'tmp' . DS);
+define('CACHE', TMP);
+define('LOGS', TMP);
 
-require ROOT . '/vendor/cakephp/cakephp/src/basics.php';
-require ROOT . '/vendor/autoload.php';
+$loader = new \Cake\Core\ClassLoader;
+$loader->register();
+$loader->addNamespace('Cake\Test\Fixture', ROOT . '/vendor/cakephp/cakephp/tests/Fixture');
 
-Cake\Core\Configure::write('App', ['namespace' => 'Proffer\Tests']);
-Cake\Core\Configure::write('debug', true);
+require_once CORE_PATH . 'config/bootstrap.php';
 
-$TMP = new \Cake\Filesystem\Folder(TMP);
-$TMP->create(TMP . 'cache/models', 0777);
-$TMP->create(TMP . 'cache/persistent', 0777);
-$TMP->create(TMP . 'cache/views', 0777);
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
 
-$cache = [
-	'default' => [
-		'engine' => 'File'
-	],
-	'_cake_core_' => [
-		'className' => 'File',
-		'prefix' => 'crud_myapp_cake_core_',
-		'path' => CACHE . 'persistent/',
-		'serialize' => true,
-		'duration' => '+10 seconds'
-	],
-	'_cake_model_' => [
-		'className' => 'File',
-		'prefix' => 'crud_my_app_cake_model_',
-		'path' => CACHE . 'models/',
-		'serialize' => 'File',
-		'duration' => '+10 seconds'
+Configure::write('debug', true);
+
+Configure::write('App', [
+	'namespace' => 'App',
+	'encoding' => 'UTF-8',
+	'base' => false,
+	'baseUrl' => false,
+	'dir' => 'src',
+	'webroot' => 'webroot',
+	'www_root' => APP . 'webroot',
+	'fullBaseUrl' => 'http://localhost',
+	'imageBaseUrl' => 'img/',
+	'jsBaseUrl' => 'js/',
+	'cssBaseUrl' => 'css/',
+	'paths' => [
+		'plugins' => [APP . 'Plugin' . DS],
+		'templates' => [APP . 'Template' . DS]
 	]
-];
+]);
 
-Cake\Cache\Cache::config($cache);
-Cake\Core\Configure::write('Session', [
+Configure::write('Session', [
 	'defaults' => 'php'
 ]);
 
-Cake\Core\Plugin::load('Proffer');
-
-Cake\Routing\DispatcherFactory::add('Routing');
-Cake\Routing\DispatcherFactory::add('ControllerFactory');
+Cache::config([
+	'_cake_core_' => [
+		'engine' => 'File',
+		'prefix' => 'cake_core_',
+		'serialize' => true
+	],
+	'_cake_model_' => [
+		'engine' => 'File',
+		'prefix' => 'cake_model_',
+		'serialize' => true
+	],
+	'default' => [
+		'engine' => 'File',
+		'prefix' => 'default_',
+		'serialize' => true
+	]
+]);
 
 // Ensure default test connection is defined
-if (!getenv('db_dsn')) {
-	putenv('db_dsn=sqlite:///:memory:');
+if (!getenv('db_class')) {
+	putenv('db_class=Cake\Database\Driver\Sqlite');
+	putenv('db_dsn=sqlite::memory:');
 }
 
-Cake\Datasource\ConnectionManager::config('test', [
-	'url' => getenv('db_dsn'),
+ConnectionManager::config('test', [
+	'className' => 'Cake\Database\Connection',
+	'driver' => getenv('db_class'),
+	'dsn' => getenv('db_dsn'),
+	'database' => getenv('db_database'),
+	'username' => getenv('db_login'),
+	'password' => getenv('db_password'),
 	'timezone' => 'UTC'
 ]);
+
+Log::config([
+	'debug' => [
+		'engine' => 'Cake\Log\Engine\FileLog',
+		'levels' => ['notice', 'info', 'debug'],
+		'file' => 'debug',
+	],
+	'error' => [
+		'engine' => 'Cake\Log\Engine\FileLog',
+		'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+		'file' => 'error',
+	]
+]);
+
+Plugin::load('Proffer', ['path' => ROOT]);
